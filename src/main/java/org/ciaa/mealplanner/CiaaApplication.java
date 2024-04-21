5125ecc69a8905a8c30b4f6c307b32f211e8ae9c
copetan <copetanbird@gmail.com>
2024-04-21 16:13:20 -0700

    fix build error

--- src/main/java/org/ciaa/mealplanner/CiaaApplication.java
+++ src/main/java/org/ciaa/mealplanner/CiaaApplication.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 /**
  * The class containing the main method responsible for running the application.
  */
